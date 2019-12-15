678e5c5a24cc21e00484de33d619a1bb4d29a049
Vincent <0965190@hr.nl>
2019-12-16 00:39:21 +0100

    made the crud for retrospectives

---
+++ resources/views/pages/retrospectivecreate.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app', ['page' => __('Create a Retrospective'), 'pageSlug' => 'notifications'])
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+      <form method="POST" action="/retrospective/store" enctype="multipart/form-data">
+        @csrf   
+        <div>
+            <label for="file">Upload retrospective file</label><br/>
+            <input id="file" name="file" type="file" name="..." required  />
+        </div>
+        <br/>
+        <div class="form-group">
+            <label for="outcome">General outcome</label>
+            <input type="text" class="form-control" name="outcome" id="outcome" placeholder="Type here the general outcome" required>
+        </div>
+        <div class="form-group">
+            <label for="sprint">Sprint number</label>
+            <select class="form-control" name="sprintNumber" id="sprint" required>
+                @foreach($sprintNumbers as $sprintnumber)
+                <option style="color:black;">{{$sprintnumber}}</option>
+                @endforeach
+            </select>
+        </div>
+        <input type="hidden"name="invisible" value="{{$project_id}}">
+        <button type="submit" class="btn btn-primary">Create</button>
+      </form>
+    </div>
+  </div>
+@endsection
--- resources/views/pages/retrospectives.blade.php
+++ resources/views/pages/retrospectives.blade.php
@@ -9,15 +9,40 @@
                     <tr>
                         <th>Sprint number</th>
                         <th>Outcome</th>
+                        <th>File</th>
+                        @if($admin == true)
+                        <th>Actions</th>
+                        @endif
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($data as $data)
                         <tr>
-                        <td>{{ $data[0] }}</td>
-                        <td>{{ $data[1] }}</td>
+                            <td>{{ $data[0] }}</td>
+                            <td>{{ $data[1] }}</td>
+                            <td>
+                                @if($data[2] != null)
+                                <a href="/files/{{$data[2]}}">Download</a>
+                                @else
+                                No file
+                                @endif
+                            </td>
+                        @if($admin == true)
+                        <td>
+                            <a href="/retrospective/delete/{{ $data[3] }}" onclick="return confirm('Are you sure?')" class="btn btn-danger">Delete</a>
+                            <a href="/retrospective/edit/{{ $data[3] }}" class="btn btn-success">Edit</a>
+                        </td>
+                        @endif
                         </tr>
                     @endforeach
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            @if($admin == true)
+                            <td></td>
+                            @endif
+                            <td><a href="{{ url('retrospective/create/' . $project_id) }}" class="btn btn-primary">New</a></td>
+                        </tr>
                 </tbody>
             </table>
         </div>
