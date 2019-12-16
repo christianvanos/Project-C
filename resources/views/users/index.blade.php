093d88717e473806ff520ee4e69ec8e87e67a254
christianvanos <47158164+christianvanos@users.noreply.github.com>
2019-12-16 12:06:23 +0100

    qwertyuiop

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['page' => __('User Management'), 'pageSlug' => 'members' . $project_id])
+@extends('layouts.app', ['page' => __('User Management'), 'pageSlug' => 'members_' . $project->id])
 
 @section('content')
     <div class="row">
