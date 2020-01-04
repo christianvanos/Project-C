994b8465046b01d1c5413d99b8ad5c0f0c1a785f
christianvanos <47158164+christianvanos@users.noreply.github.com>
2020-01-04 20:55:49 +0100

    update to seeder

--- database/seeds/SprintsTableSeeder.php
+++ database/seeds/SprintsTableSeeder.php
@@ -18,32 +18,32 @@ class SprintsTableSeeder extends Seeder
             DB::table('sprints')->insert([
                 'number' => 1,
                 'project_id' => App\Projects::find($id)->id,
-                'start_date' => Carbon::now()->subDays(56),
-                'end_date' => Carbon::now()->subDays(42),
+                'start_date' => Carbon::now()->subDays(45),
+                'end_date' => Carbon::now()->subDays(31),
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
             DB::table('sprints')->insert([
                 'number' => 2,
                 'project_id' => App\Projects::find($id)->id,
-                'start_date' => Carbon::now()->subDays(42),
-                'end_date' => Carbon::now()->subDays(28),
+                'start_date' => Carbon::now()->subDays(31),
+                'end_date' => Carbon::now()->subDays(17),
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
             DB::table('sprints')->insert([
                 'number' => 3,
                 'project_id' => App\Projects::find($id)->id,
-                'start_date' => Carbon::now()->subDays(28),
-                'end_date' => Carbon::now()->subDays(14),
+                'start_date' => Carbon::now()->subDays(17),
+                'end_date' => Carbon::now()->subDays(3),
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
             DB::table('sprints')->insert([
                 'number' => 4,
                 'project_id' => App\Projects::find($id)->id,
-                'start_date' => Carbon::now()->subDays(14),
-                'end_date' => now(),
+                'start_date' => Carbon::now()->subDays(3),
+                'end_date' => Carbon::now()->addDays(11),
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
