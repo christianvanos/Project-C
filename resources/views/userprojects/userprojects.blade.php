d606d94eb02492b41ac3c0ca3c1b66d2ac099fc6
Vincent030299 <vincentschool0302@hotmail.com>
2020-01-07 14:30:45 +0100

    Styling for adding user to a project

--- resources/views/userprojects/userprojects.blade.php
+++ resources/views/userprojects/userprojects.blade.php
@@ -3,10 +3,22 @@
 @section('content')
 
 <div class="card">
+   <div class="card-body">
+      @if (session('status'))
+          <div class="alert alert-success" role="alert">
+              {{ session('status') }}
+          </div>
+      @elseif (session('error'))
+          <div class="alert alert-danger" role="alert">
+              {{ session('error') }}
+          </div>
+      @endif
+      <div class="card-header">
+         <h4 class="card-title">Adding a user to a project</h4>
+      </div>
       <div class="card-body">
         <form action="create" method="POST">
            <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
-           <h1>Adding user to a project</h1>
             <div class="form-group">
                   <label for="users">Users</label>
                   <select class="form-control" name="users" id="users">
